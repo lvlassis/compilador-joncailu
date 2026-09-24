8ffdf3c7787a2d1eab087499a1d32c8cf5f3266c
lvinicius <lvinicius@tce.go.gov.br>
2026-09-24 14:48:43 -0300

    docs: adiciona comentários na função lexic_read_token, para ajudar a implementar

--- src/lexic.c
+++ src/lexic.c
@@ -2,7 +2,18 @@
 
 // Função que retorna o próximo token, consumindo parte da string
 Token lexic_read_token(CompilerContext* ctx) {
-    int pos_inicial = ctx->current_pos;
+    int cursor = ctx->current_pos;          // Posição do cursor na string
+    char value = ctx->source_code[cursor]   // Valor do cursor
+    
+    // Implementar lógica de percorrer a string do código fonte para 
+    // identificar onde inicia e termina o próximo lexema
+    
+    // Implementar lógica de classificar o lexema selecionado 
+    // gerando um token.
+    
+    // Mover o ctx->current_pos; para o final do lexema identificado
+
+    // (MOCK) Retorna sempre token EOF
     Token t = {TokenEOF, ""};
     return t;
 }
