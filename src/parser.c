2612ef209ab97add3c6561b6a343471c9812b4de
Jonathas dos Santos <jonathasdossantos007@gmail.com>
2026-09-24 19:31:34 -0300

    Fix security issues in parser.c

--- src/parser.c
+++ src/parser.c
@@ -8,8 +8,11 @@ void parser_parse(CompilerContext *ctx) {
     Token t;
     do  {
         t = lexic_read_token(ctx);
+        if (t.type == TokenNULL)
+            break;
         if (t.type != TokenEOF) {
             token_print(&t);
+            free_token(&t);
         }
     } while (t.type != TokenEOF);
 }
