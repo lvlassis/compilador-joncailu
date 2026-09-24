36cf686543678475d6bc0eec155f47d349c1de33
Lucas Vinícius de L. Assis <lvlassis.2@gmail.com>
2026-09-24 19:47:19 -0300

    ci: teste código que compila

--- src/automaton.c
+++ src/automaton.c
@@ -6,8 +6,7 @@
 
 TokenType handle_state_transition(StateType *state, char caracter)
 {
-// :D
-    StateType newState = Unknown
+    StateType newState = Unknown;
     TokenType probTokenType;
     TokenType ignToken = TokenNULL;
     switch (caracter) {
