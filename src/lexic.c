4f339da34e860080fc00d9a2da3c6bec7f20e7aa
Jonathas dos Santos <jonathasdossantos007@gmail.com>
2026-09-24 19:30:25 -0300

    Implement lexic.c logic

--- src/lexic.c
+++ src/lexic.c
@@ -1,20 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "include/lexic.h"
 #include "include/token.h"
+#include "include/automaton.h"
 
 // Função que retorna o próximo token, consumindo parte da string
 Token lexic_read_token(CompilerContext* ctx) {
     int cursor = ctx->current_pos;          // Posição do cursor na string
+    int auxCursor = cursor;
     char value = ctx->source_code[cursor];   // Valor do cursor
-    
-    // Implementar lógica de percorrer a string do código fonte para 
-    // identificar onde inicia e termina o próximo lexema
-    
-    // Implementar lógica de classificar o lexema selecionado 
-    // gerando um token.
-    
-    // Mover o ctx->current_pos; para o final do lexema identificado
+    StateType state = Start;                // Estado do autômato
+    TokenType tokenType = TokenNULL;
 
-    // (MOCK) Retorna sempre token EOF
-    Token t = {TokenID, "a"};
+    for (int i = 0; tokenType == TokenNULL || tokenType == TokenIgn; i++) 
+    {
+        auxCursor = cursor + i;
+        value = ctx->source_code[auxCursor];
+
+        tokenType = handle_state_transition(&state, value);
+        if (tokenType == TokenIgn) i++;
+        printf("NewState: %d\n", state);
+        printf("TokenType: %d\n\n", tokenType);
+        if (state == Error)
+        {
+            printf("Error!");
+            break;
+        }
+    }
+
+    char *charCopy = (char *)malloc((auxCursor + 1) * sizeof(char));
+    
+    if (charCopy == NULL) {
+        printf("Erro de alocação de memória.\n");
+    }
+
+    strncpy(charCopy, ctx->source_code, auxCursor);
+    charCopy[auxCursor] = '\0';
+    
+    ctx->source_code += auxCursor;
+    Token t = {tokenType, charCopy};
     return t;
 }
