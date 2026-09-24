f1cb1aa2d8ef183e12b74cff6f7b08b7f0b01b68
Jonathas dos Santos <jonathasdossantos007@gmail.com>
2026-09-23 22:32:44 -0300

    Add token.c file with 'lookup token' functionality

---
+++ scanner/src/token.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "include/token.h"
+
+
+static int compare_keywords(const void *word, const void *element)
+{
+    const char *searched_word = (const char *) word;
+    const Keyword *kw = (const Keyword*) element;
+    return strcmp(searched_word, kw->lexeme);
+}
+
+TokenType lookup_token(const char *lexeme)
+{
+    Keyword *match = bsearch(
+        lexeme,
+        KEYWORDS,
+        NUM_KEYWORDS,
+        sizeof(Keyword),
+        compare_keywords
+    );
+
+    if (match != NULL)
+    {
+        return match->type;
+    }
+
+    return TokenID;
+}
